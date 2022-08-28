081e90af69adec146d38f9fb74f1a7200d0ecbcc
daramayo90 <da.aramayo1990@gmail.com>
2022-08-28 13:40:37 -0500

    mongoose scheme created called EntryModel

---
+++ nextjs/03-trello/models/Entry.ts
@@ -0,0 +1,22 @@
+// This code is server-side rendering
+import mongoose, { Model, Schema } from 'mongoose';
+import { Entry } from '../interfaces';
+
+// Check 'entry.ts'
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ['pending', 'in-progress', 'finished'],
+      message: '{VALUE} is not an allowed state',
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', entrySchema);
+
+export default EntryModel;
